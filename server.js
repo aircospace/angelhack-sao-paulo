02d4bdf4552a300513ecd9c0631058fd802f51b4
Victor Kazuyuki Shinya <contatovictorshinya@gmail.com>
2017-07-30 01:23:38 -0300

    Server.js and .env created and configured

---
+++ server.js
@@ -0,0 +1,24 @@
+// https://github.com/expressjs/express
+const express = require('express');
+// https://nodejs.org/api/http.html
+const http = require('http');
+// https://github.com/Automattic/mongoose
+const mongoose = require('mongoose');
+// https://github.com/motdotla/dotenv
+const dotenv = require('dotenv').load();
+// https://github.com/expressjs/body-parser
+const bodyParser = require('body-parser');
+
+mongoose.connect(process.env.MONGODB, {
+    useMongoClient: true
+});
+mongoose.connection.once('Connection error', console.error);
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+routes.router(app);
+
+return http.createServer(app).listen(process.env.PORT || 3000, () => {
+    console.log(`Server running at port 3000`);
+});
